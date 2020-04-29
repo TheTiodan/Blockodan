ad1c11ac8dca9bb865d335e14802c170b7eb27da
TheTiodan <timo-kleinschnitz@gmx.de>
2020-04-29 15:02:42 +0200

    Updatetest

---
+++ Assets/Credits.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject panel;
+    public void OpenCredits()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(true);
+        }
+    }
+    public void CloseCredits()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public GameObject Panel;
+    public void ShowOptions()
+    {
+        if(Panel !=null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+    public void CloseOptions()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
